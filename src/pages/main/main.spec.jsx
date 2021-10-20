f9a23bd80aac08a3324411897aeef05c26ab1ccb
denexwstoniliompira <denexwstoniliompira@jsmate.net>
2021-10-20 22:35:11 +0200

    Fix tests

--- src/pages/main/main.spec.jsx
+++ src/pages/main/main.spec.jsx
@@ -4,13 +4,20 @@ import { mount } from '@cypress/react'
 import Main from './main'
 import { FETCH_REPOSITORIES } from '../../api-endpoints'
 
+import { IntlProvider } from 'react-intl'
+import en from '../../translations/en.json'
+
 describe('Main', () => {
     it('should render loading indicator', () => {
         cy.intercept('GET', FETCH_REPOSITORIES, {
             statusCode: 401,
         }).as('getReposListWithError')
 
-        mount(<Main />)
+        mount(
+            <IntlProvider locale="en" messages={en}>
+                <Main />
+            </IntlProvider>
+        )
         cy.get('[testid=loading-indicator]').should('exist')
     })
 
@@ -19,7 +26,11 @@ describe('Main', () => {
             statusCode: 401,
         }).as('getReposListWithError')
 
-        mount(<Main />)
+        mount(
+            <IntlProvider locale="en" messages={en}>
+                <Main />
+            </IntlProvider>
+        )
         cy.wait('@getReposListWithError')
             .get('div[role=alert]')
             .contains('Something went wrong')
@@ -28,7 +39,11 @@ describe('Main', () => {
     it('should render correct table header cells', () => {
         cy.intercept(FETCH_REPOSITORIES).as('getReposList')
 
-        mount(<Main />)
+        mount(
+            <IntlProvider locale="en" messages={en}>
+                <Main />
+            </IntlProvider>
+        )
 
         cy.wait('@getReposList')
             .get('thead')
